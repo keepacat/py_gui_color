20ea0d082d24e60c5d10e2ad97a70df56db503d5
keep_a_cat <1719215179@qq.com>
2022-07-21 09:45:11 +0800

    update app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+#!/usr/bin/env python3
+#coding=utf-8
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtCore import QTimer, QPoint, QFile, QIODevice, QTextCodec, QRect, QPointF, Qt
+from PyQt5.QtGui import QPainter, QColor, QPolygonF, QPainterPath, QBrush, QPen, QFont, QIcon
 import sys
+import os
 
 class ColorWidget(QWidget):
     def __init__(self, parent=None):
@@ -11,26 +14,32 @@ class ColorWidget(QWidget):
         self.timer.timeout.connect(self.onTimer)
         self.index = 0
         self.colors = []
+        self.mpos = QPoint()
+        self.mouse = QPoint()
 
     def initWidget(self):
         self.resize(960, 680)
 
     def start(self):
         path = sys.argv[0]
-        pathColor = path.replace('app.py', 'color.txt')
+        pathColor = path.replace('app.py', 'color_utf8.txt')
+        # pathColor = os.path.dirname(sys.executable) + '\\color_utf8.txt'
         file = QFile(pathColor)
         if file.open(QIODevice.ReadOnly | QIODevice.Text):
-            fileData = str(file.readAll(), encoding='utf8')
+            fileWord = file.readAll().data()
+            fileData = fileWord.decode(encoding="utf-8", errors="ignore")
             itmes = fileData.split('\n\n')
             for item in itmes:
-                colors = {}
                 tmps = item.split('\n')
+                colors = {}
                 for tmp in tmps:
                     t = tmp.split(':')
-                    colors[t[0]] = t[1]
-                self.colors.append(colors)
+                    if t.__len__() == 2:
+                        colors[t[0]] = t[1]
+                if colors.__len__() > 0:
+                    self.colors.append(colors)
             file.close()
-        # self.timer.start(1000)
+        self.timer.start(2000)
 
     def onTimer(self):
         self.index += 1
@@ -39,14 +48,37 @@ class ColorWidget(QWidget):
         self.repaint()
 
     def mousePressEvent(self, event):
-        self.onTimer()
         super().mousePressEvent(event)
+        self.mpos = self.pos()
+        self.mouse = event.globalPos()
 
     def mouseMoveEvent(self, event):
         super().mouseMoveEvent(event)
+        pos = self.mpos + event.globalPos() - self.mouse
+        self.move(pos)
 
     def mouseReleaseEvent(self, event):
         super().mouseReleaseEvent(event)
+
+    def keyPressEvent(self, event):
+        super().keyPressEvent(event)
+        if event.key() == Qt.Key_Q:
+            self.close()
+        elif event.key() == Qt.Key_Space:
+            if self.timer.isActive() :
+                self.timer.stop()
+            else:
+                self.timer.start()
+        elif event.key() == Qt.Key_Left:
+            self.index -= 1
+            if 0 > self.index :
+                self.index = self.colors.__len__() - 1
+            self.repaint()
+        elif event.key() == Qt.Key_Right:
+            self.index += 1
+            if self.colors.__len__() == self.index :
+                self.index = 0
+            self.repaint()
 
     def paintEvent(self, event):
         if self.colors.__len__() <= self.index:
@@ -54,31 +86,77 @@ class ColorWidget(QWidget):
         items = self.colors[self.index]
         width = self.width()
         height = self.height()
-        painter = QPainter(self)
-        brush = QBrush(QColor(items['hex']))
-        painter.setBrush(brush)
-        painter.drawRect(QRect(0, 0, width, height))
+        color = QColor(items['hex'])
+        rect = QRect(0, 0, width, height)
 
-        pen = QPen(QColor('#ffffff'))
-        painter.setPen(pen)
-        font = QFont('仿宋', 80, 3)
-        painter.setFont(font)
-        painter.drawText(QRect(0, 0, width, height), Qt.AlignCenter, items['name'])
+        painter = QPainter(self)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QBrush(color))
+        painter.drawRect(rect)
+
+        polygon = QPolygonF()
+        polygon.append(QPointF(width * 2 / 3, 0))
+        polygon.append(QPointF(width, height / 3))
+        polygon.append(QPointF(width, 0))
+
+        polygon2 = QPolygonF()
+        polygon2.append(QPointF(0, height * 2 / 3))
+        polygon2.append(QPointF(width / 3, height))
+        polygon2.append(QPointF(0, height))
+
+        paintPath = QPainterPath()
+        paintPath.addPolygon(polygon)
+        paintPath.addPolygon(polygon2)
+        paintPath.closeSubpath()
+
+        color2 = QColor("#ffffff")
+        color2.setRed(min(color.red() + 30, 255))
+        color2.setGreen(min(color.green() + 30, 255))
+        color2.setBlue(min(color.blue() + 30, 255))
+        painter.setBrush(QBrush(color2))
+        painter.drawPath(paintPath)
+
+        cTmep = color.red() + color.green() + color.blue()
+        if  cTmep > 360:
+            color2.setRed(max(color.red() - 120, 0))
+            color2.setGreen(max(color.green() - 120, 0))
+            color2.setBlue(max(color.blue() - 120, 0))
+        else:
+            color2.setRed(min(color.red() + 120, 255))
+            color2.setGreen(min(color.green() + 120, 255))
+            color2.setBlue(min(color.blue() + 120, 255))
         
-        font = QFont('仿宋', 20, 3)
+        painter.setPen(QPen(color2))
+        font = QFont('仿宋', 80)
         painter.setFont(font)
-        header = str(self.index + 1) + '/' + str(self.colors.__len__()) + '\n'
-        painter.drawText(QRect(0, 0, width, height), Qt.AlignLeft | Qt.AlignTop, header)
-        footer = items['describe'] + '\n' + items['author'] + ' -- 《' + items['book'] + '》'
-        painter.drawText(QRect(0, 0, width, height), Qt.AlignRight | Qt.AlignBottom, footer)
+        painter.drawText(rect, Qt.AlignCenter, items['name'])
+        
+        font = QFont('仿宋', 14)
+        painter.setFont(font)
+        rect.adjust(10, 10, -10, -10)
+        header = 'R' + str(color.red()) + ' G' + str(color.green()) + ' B' + str(color.blue())
+        painter.drawText(rect, Qt.AlignLeft | Qt.AlignTop, header)
+        footer = ''
+        if items['book'].__len__() > 0:
+            footer += '《' + items['book'] + '》'
+        if items['author'].__len__() > 0:
+            footer += ' -- ' + items['author']
+        if items['describe'].__len__() > 0:
+            footer += '\n' + items['describe']
+        painter.drawText(rect, Qt.AlignRight | Qt.AlignBottom, footer)
         super().paintEvent(event)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    codec = QTextCodec.codecForName("UTF-8")
-    QTextCodec.setCodecForLocale(codec)
+#main
+app = QApplication(sys.argv)
+codec = QTextCodec.codecForName("utf-8")
+QTextCodec.setCodecForLocale(codec)
 
-    w = ColorWidget()
-    w.show()
-    w.start()
-    sys.exit(app.exec_())
+# path = sys.argv[0]
+# pathIcon = path.replace('app.py', 'logo.ico')
+
+w = ColorWidget()
+w.setWindowFlags(Qt.FramelessWindowHint)
+# w.setWindowIcon(QIcon(pathIcon))
+w.show()
+w.start()
+sys.exit(app.exec_())
